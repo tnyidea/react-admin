53d55f3588263ab8f58f083d5a9dbd3e29f1f2e3
gbnyc26 <gregbehrend26@gmail.com>
2022-07-28 17:16:30 -0400

    Remove unused code

--- dataprovider/go/main.go
+++ dataprovider/go/main.go
@@ -1,16 +1,4 @@
 package main
 
-import (
-	"github.com/tnyidea/react-admin-dataprovider/go/httpserver"
-	"log"
-)
-
 func main() {
-	server, err := httpserver.NewHttpServer(8080)
-	if err != nil {
-		log.Fatal("fatal: error initializing server: ", err)
-	}
-	defer server.Defer()
-
-	log.Fatal(server.ListenAndServe())
 }
