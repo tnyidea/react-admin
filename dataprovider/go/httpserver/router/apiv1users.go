df4a582b5536ef3d5e02e8c27ac670349f3befdb
gbnyc26 <gregbehrend26@gmail.com>
2022-08-03 08:53:13 -0400

    Refactored endpoint call for FindAllUsersV1

--- dataprovider/go/httpserver/router/apiv1users.go
+++ dataprovider/go/httpserver/router/apiv1users.go
@@ -6,7 +6,7 @@ import (
 	"github.com/tnyidea/go-httpserver/sample/userservice/httpserver/config"
 	"github.com/tnyidea/go-sample-userdata/models"
 	"github.com/tnyidea/react-admin/dataprovider/go/httpserver/endpoints"
-	httpresponse "github.com/tnyidea/react-admin/dataprovider/go/httpserver/response"
+	"github.com/tnyidea/react-admin/dataprovider/go/httpserver/response"
 	"net/http"
 )
 
@@ -14,7 +14,7 @@ func AddApiV1UsersRouter(router *mux.Router, ctx context.Context) *mux.Router {
 	db := ctx.Value(config.UserServiceContextDatabase).(models.DB)
 
 	router.HandleFunc("/api/v1/users", func(w http.ResponseWriter, r *http.Request) {
-		httpresponse.WriteDataProviderResponseData(w, r, endpoints.FindAllUsersV1(r, db))
+		response.WriteDataProviderResponseData(w, r, endpoints.FindAllUsersV1(r, db))
 	}).Methods(http.MethodGet)
 
 	return router
